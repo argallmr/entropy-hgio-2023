39d3c1e60ec42042af816e1b588dceaadb86fa23
Hanieh Karimi <132312837+krmhanieh@users.noreply.github.com>
2024-03-21 20:52:14 -0400

    Update De_function.py

--- De_function.py
+++ De_function.py
@@ -30,7 +30,7 @@ def curl(K, V):
         return curl
 
 
-def De(sc,mode,species,t0,t1):
+def De_total(sc,mode,species,t0,t1):
     optdesc = 'd{0}s-moms'.format(species)
     #should it be brst or srvy?
     data = fpi.load_moms(sc=sc, mode=mode, optdesc=optdesc, start_date=t0, end_date=t1)
@@ -74,15 +74,15 @@ def De(sc,mode,species,t0,t1):
 
     fig, axes = plt.subplots(nrows=4, ncols=1, squeeze=False)
 
-# Current Density
+
     ax = axes[0,0]
     D.loc[:,'x'].plot(ax=ax, label='x')
     D.loc[:,'y'].plot(ax=ax, label='y')
     D.loc[:,'z'].plot(ax=ax, label='z')
-    ax.set_title('Application of Reciprocal Vectors')
+    ax.set_title('Electron Frame Dissipation Rate')
     ax.set_xlabel('')
     ax.set_xticklabels([''])
-    ax.set_ylabel('J [$\\mu A/m^{2}$]')
+    ax.set_ylabel('De [$\\mu A/m^{2}$]')
     ax.legend()
     ax = axes[1,0]
     D.loc[:,'x'].plot(ax=ax, label='x')
@@ -138,10 +138,10 @@ def De(sc,mode,species,t0,t1):
     D1.loc[:,'x'].plot(ax=ax, label='x')
     D1.loc[:,'y'].plot(ax=ax, label='y')
     D1.loc[:,'z'].plot(ax=ax, label='z')
-    ax.set_title('Application of Reciprocal Vectors')
+    ax.set_title('Electron Frame Dissipation Rate')
     ax.set_xlabel('')
     ax.set_xticklabels([''])
-    ax.set_ylabel('J [$\\mu A/m^{2}$]')
+    ax.set_ylabel('De [$\\mu A/m^{2}$]')
     ax.legend()
     ax = axes[1,0]
     D1.loc[:,'x'].plot(ax=ax, label='x')
@@ -153,10 +153,10 @@ def De(sc,mode,species,t0,t1):
     D.loc[:,'x'].plot(ax=ax, label='x')
     D.loc[:,'y'].plot(ax=ax, label='y')
     D.loc[:,'z'].plot(ax=ax, label='z')
-    ax.set_title('Application of Reciprocal Vectors')
+    ax.set_title('Electron Frame Dissipation Rate')
     ax.set_xlabel('')
     ax.set_xticklabels([''])
-    ax.set_ylabel('J [$\\mu A/m^{2}$]')
+    ax.set_ylabel('De [$\\mu A/m^{2}$]')
     ax.legend()
     ax = axes[1,1]
     D.loc[:,'x'].plot(ax=ax, label='x')
@@ -168,3 +168,73 @@ def De(sc,mode,species,t0,t1):
 #t0 = dt.datetime(2017, 7, 11, 22, 33, 30)
 #t1 = dt.datetime(2017, 7, 11, 22, 34, 30)
 #De('mms1','brst','e',t0,t1)
+
+
+
+#for a single plot
+def De_single(sc,mode,species,t0,t1):
+    optdesc = 'd{0}s-moms'.format(species)
+    #should it be brst or srvy?
+    data = fpi.load_moms(sc=sc, mode=mode, optdesc=optdesc, start_date=t0, end_date=t1)
+    data['velocity']
+    b= fgm.load_data(sc=sc, mode=mode, start_date=t0, end_date=t1)
+
+    b['B_GSE']
+#brst of srvy for edp?
+    e= edp.load_data(sc=sc, mode='srvy', start_date=t0, end_date=t1)
+#e['E_GSE']
+
+    def curl(K, V):
+        curl = 0
+    
+        for k_name, v_name in zip(K, V):
+            k = K[k_name]
+            v = V[v_name]
+            curl += xr.concat([k[:,1]*v[:,2] - k[:,2]*v[:,1],
+                           k[:,2]*v[:,0] - k[:,0]*v[:,2],
+                           k[:,0]*v[:,1] - k[:,1]*v[:,0]], dim='component').transpose()
+    
+        curl = curl.assign_coords({'component': ['x', 'y', 'z']})
+        return curl
+#should this one then be linear?
+    b1 = b['B_GSE'].interp_like(e, method='linear')
+    data1 =data.interp_like(e, method='nearest')
+    B = xr.Dataset({'B1': b1})
+    U = xr.Dataset({'U1': data1['velocity'].rename({'velocity_index': 'component'}).assign_coords({'component': ['x', 'y', 'z']})})
+    B2 = 1e-9 * B
+    U2 = 1e-3 * U
+    curlB = curl(U2, B2)
+#A = 1e-12 * curlB
+
+    e1=e['E_GSE']
+    #Error in here?!!
+    E = xr.Dataset({'E1': e1.rename({''.join(sc)+'_edp_label1_fast_l2': 'component'}).assign_coords({'component': ['x', 'y', 'z']})})
+
+    D= E['E1'] + curlB
+
+    D #(just shows how the data looks like)
+
+    fig, axes = plt.subplots(nrows=1, ncols=1, squeeze=False)
+
+
+    ax = axes[0,0]
+    D.loc[:,'x'].plot(ax=ax, label='x')
+    D.loc[:,'y'].plot(ax=ax, label='y')
+    D.loc[:,'z'].plot(ax=ax, label='z')
+    ax.set_title('Electron Frame Dissipation Rate')
+    ax.set_xlabel('')
+    ax.set_xticklabels([''])
+    ax.set_ylabel('De [$\\mu A/m^{2}$]')
+    ax.legend()
+
+    plt.show()
+#D=D.to_array()
+
+#D=D.T
+
+
+
+#t0 = dt.datetime(2017, 7, 11, 22, 33, 30)
+#t1 = dt.datetime(2017, 7, 11, 22, 34, 30)
+#De('mms1','brst','e',t0,t1)
+
