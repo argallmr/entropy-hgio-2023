Use new LUT to plot P_HORNET * FIX: database- 'units' attribute interferes with how xarray writes netCDF files; improved error messags * UDPATE: maxlut- create __main__-level script; include relative entropy in output * NEW: physics- functions for calculating/plotting permutation entropy and complexity * UPDATE: plots- use the new LUT files when plotting P_HORNET

database.py
@@ -106,14 +106,14 @@ def load_data(t0, t1, mode='brst', dt_out=np.timedelta64(30, 'ms')):
     mec_data = get_data('mec', mode, t0, t1, dt_out)
 
     # Combine into a single dataset
-    try:
-        data = xr.merge([des_data, dis_data, edi_data,
-                         edp_data, fgm_data, mec_data])
-    except:
-        import pdb
-        pdb.set_trace()
+    data = xr.merge([des_data, dis_data, edi_data,
+                        edp_data, fgm_data, mec_data])
 
     # Save to data file
+    if 'units' in data['time'].attrs:
+        data['time'].attrs['UNITS'] = data['time'].attrs['units']
+        del data['time'].attrs['units']
+
     data.to_netcdf(fname)
 
     return fname
@@ -183,8 +183,11 @@ def get_data(instr, mode, t0, t1, dt_out=np.timedelta64(30, 'ms')):
         # Load the data
         try:
             data = func(sc, mode, t0, t1)
-        except:
-            print('No data for {0}_{1}_{2}_{3}_{4}'.format(sc, instr, mode, t0, t1))
+        except Exception as E:
+            print('Error processing {0} {1} {2} during interval {3} - {4}'
+                  .format(sc, instr, mode, t0, t1))
+            print('\t{0}'.format(type(E)))
+            print('\t{0}'.format(E))
             continue
 
         # Resample the data to a common time stamp

maxlut.py
@@ -923,9 +923,6 @@ class Lookup_Table():
         # 2D V-Space Entropy LUT: sV vs. n
         #
 
-        import pdb
-        pdb.set_trace()
-
         ax = axes[2,0]
         self._plot(ax, sV_lut, n, n_EM, n_M, sV, sV_EM, sV_M,
                    xx=self.n_lut, yy=sV_lut)
@@ -1081,22 +1078,8 @@ def plot_max_lut(data):
     return fig, axes
 
 
-def main_ts():
+def main_ts(sc, mode, optdesc, t0, t1):
 
-    #
-    # Define the dataset
-    #
-    
-    # Time at which to select the distribution
-    ti = np.datetime64('2017-07-11T22:34:02.5')
-
-    # Define some input parameters
-    sc = 'mms4'
-    mode = 'brst'
-    optdesc = 'des-dist'
-    t0 = dt.datetime(2017, 7, 11, 22, 34, 2)
-    t1 = dt.datetime(2017, 7, 11, 22, 34, 3)
-    species = optdesc[1]
 
     #
     # Check if we already ran this interval
@@ -1140,6 +1123,7 @@ def main_ts():
     t_M = fpi.scalar_temperature(f_M)
     s_M = fpi.entropy(f_M)
     sv_M = fpi.vspace_entropy(f_M, n=n_M, s=s_M)
+    sv_rel_M = fpi.relative_entropy(des_pre, f_M)
 
     #
     # Make lists to gether output data
@@ -1151,6 +1135,7 @@ def main_ts():
     t_lut = list()
     s_lut = list()
     sv_lut = list()
+    sv_rel_lut = list()
 
     n = list()
     t = list()
@@ -1198,9 +1183,10 @@ def main_ts():
         t_err = np.abs(ti - t_M[idx]) / ti   # is this smaller than ∆t/t?
         if (n_err <= deltan_n) and (t_err <= deltat_t):
             print('Equivalent Maxwellian is good enough. Do not apply LUT.')
-            f_lut.append(f_M[idx,...])
-            n_lut.append(n_M[idx])
-            t_lut.append(t_M[idx])
+            _f = fpi.Distribution_Function.from_fpi(f_M[idx,...])
+            f_lut.append(_f)
+            n_lut.append(n_M[idx].data)
+            t_lut.append(t_M[idx].data)
             
         else:
             lut = Lookup_Table(deltan_n=deltan_n, deltat_t=deltat_t, species=species)
@@ -1210,12 +1196,15 @@ def main_ts():
             #     a density and temperature most similar to the measured distribution
             _f, _n, _t = lut.apply(fi, n=ni, t=ti) # Does not exist yet
             _f = fpi.Distribution_Function.from_fpi(_f)
-            _s = _f.entropy()
             f_lut.append(_f)
             n_lut.append(_n)
             t_lut.append(_t)
-            s_lut.append(_s)
-            sv_lut.append(_f.vspace_entropy(n=_n, s=_s))
+        
+        # Calculate LUT entropy parameters
+        _s = _f.entropy()
+        s_lut.append(_s)
+        sv_lut.append(_f.vspace_entropy(n=_n, s=_s))
+        sv_rel_lut.append(fi.relative_entropy(_f))
 
     #
     # Put everything into a dataset
@@ -1231,10 +1220,12 @@ def main_ts():
                            't_M': t_M,
                            's_M': s_M,
                            'sv_M': sv_M,
+                           'sv_rel_M': sv_rel_M,
                            'n_lut': (('time',), n_lut),
                            't_lut': (('time',), t_lut),
                            's_lut': (('time',), s_lut),
-                           'sv_lut': (('time',), sv_lut)},
+                           'sv_lut': (('time',), sv_lut),
+                           'sv_rel_lut': (('time',), sv_rel_lut)},
                            coords={'time': des_dist['time']})
 
     # Thermal velocity
@@ -1256,10 +1247,11 @@ def main_ts():
 
     # Plot the results
     fig, axes = plot_max_lut(lut_data)
-    plt.show()
+    
+    return fname, fig, axes
 
 
-def main(): #sc, mode, optdesc, t0, t1, ti):
+def main(sc, mode, optdesc, t0, t1, tj):
     
     # Time at which to select the distribution
 
@@ -1313,6 +1305,7 @@ def main(): #sc, mode, optdesc, t0, t1, ti):
     t_M = f_M.scalar_temperature()
     sV_M = f_M.vspace_entropy(n=n_M)
     s_M = f_M.maxwellian_entropy(n=n_M)
+    sV_rel_M = fi.relative_entropy(f_M)
 
     # Check if they are below the grid resolution
     n_err = np.abs(ni - n_M) / ni   # is this smaller than ∆n/n?
@@ -1331,6 +1324,7 @@ def main(): #sc, mode, optdesc, t0, t1, ti):
     f_M_lut = fpi.Distribution_Function.from_fpi(f_M_lut)
     s_M_lut = f_M_lut.maxwellian_entropy(n=n_lut)
     sV_lut = f_M_lut.vspace_entropy(n=n_lut)
+    sV_rel_lut = fi.relative_entropy(f_M_lut)
 
     # Non-Maxwellanity
     Mbar_M = (sV_M - sVi) / sV_M
@@ -1373,10 +1367,100 @@ def main(): #sc, mode, optdesc, t0, t1, ti):
     print('Temperature:         {0:>7.4f}       {1:>7.4f}     {2:>7.4f}'.format(ti, t_M, t_lut))
     print('Maxwellian Entropy:  {0:2.4e}       {1:2.4e}     {2:2.4e}'.format(si_M, s_M, s_M_lut))
     print('V-Space Entropy:     {0:2.4e}       {1:2.4e}     {2:2.4e}'.format(sVi, sV_M, sV_lut))
+    print('Relative Entropy:                   {0:2.4e}     {1:2.4e}'.format(sV_rel_M, sV_rel_lut))
     print('Non-Maxwellianity:                  {0:2.4f}     {1:2.4f}'.format(Mbar_M, Mbar_lut))
 
-    plt.show()
+    return fig, axes
 
 
 if __name__ == '__main__':
-    main_ts()
+    import argparse
+    import datetime as dt
+    from os import path
+    
+    parser = argparse.ArgumentParser(
+        description='Plot an overview of MMS data.'
+        )
+    
+    parser.add_argument('sc', 
+                        type=str,
+                        help='Spacecraft Identifier')
+    
+    parser.add_argument('mode', 
+                        type=str,
+                        help='Data rate mode')
+    
+    parser.add_argument('start_date', 
+                        type=str,
+                        help='Start date of the data interval: '
+                             '"YYYY-MM-DDTHH:MM:SS""'
+                        )
+    
+    parser.add_argument('end_date', 
+                        type=str,
+                        help='End date of the data interval: '
+                             '"YYYY-MM-DDTHH:MM:SS""'
+                        )
+    
+    parser.add_argument('-t', '--time', 
+                        type=str,
+                        help='A single look-up table time: '
+                             '"YYYY-MM-DDTHH:MM:SS""'
+                        )
+                        
+    parser.add_argument('-o', '--optdesc',
+                        type=str,
+                        default='des-dist',
+                        help='Optional descriptor specifying species')
+    
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-d', '--dir',
+                       type=str,
+                       help='Path to output destination',
+                       )
+                        
+    group.add_argument('-f', '--filename',
+                       type=str,
+                       help='Output file name',
+                       )
+                        
+    parser.add_argument('-n', '--no-show',
+                        help='Do not show the plot.',
+                        action='store_true')
+
+    args = parser.parse_args()
+    t0 = dt.datetime.strptime(args.start_date, '%Y-%m-%dT%H:%M:%S')
+    t1 = dt.datetime.strptime(args.end_date, '%Y-%m-%dT%H:%M:%S')
+    tj = args.time
+    if tj is not None:
+        tj = dt.datetime.strptime(tj, '%Y-%m-%dT%H:%M:%S')
+    
+    # Create the look-up table
+    if tj is None:
+        lut_file, fig, axes = main_ts(args.sc, args.mode, args.optdesc, t0, t1)
+        print('LUT file saved to: {0}'.format(lut_file))
+    else:
+        fig, axes = main(args.sc, args.mode, args.optdesc, t0, t1, tj)
+    
+    # Save to plot to directory
+    if args.dir is not None:
+        if tj is not None:
+            fname = '_'.join((args.sc, args.optdesc[0:3], args.mode, 'l2', 'lut',
+                              tj.strftime('%Y%m%d'), tj.strftime('%H%M%S')))
+        elif t0.date() == t1.date():
+            fname = '_'.join((args.sc, args.optdesc[0:3], args.mode, 'l2', 'lut',
+                              t0.strftime('%Y%m%d'), t0.strftime('%H%M%S'),
+                              t1.strftime('%H%M%S')))
+        else:
+            fname = '_'.join((args.sc, args.optdesc[0:3], args.mode, 'l2', 'lut',
+                              t0.strftime('%Y%m%d'), t0.strftime('%H%M%S'),
+                              t1.strftime('%Y%m%d'), t1.strftime('%H%M%S')))
+        plt.savefig(path.join(args.dir, fname + '.png'))
+    
+    # Save to file
+    if args.filename is not None:
+        plt.savefig(args.filename)
+    
+    # Show on screen
+    if not args.no_show:
+        plt.show()

physics.py
@@ -278,8 +278,8 @@ def pE_rel_pt(R, sV_rel, n_M, t_M, v_str):
                         pE_rel[['sVr_n1', 'sVr_n2', 'sVr_n3', 'sVr_n4']]
                  ) # J/K/s
     pE_rel['dsV_rel_dr'] = xr.DataArray(dsV_rel_dr,
-                                    dims=('time',),
-                                    coords={'time': sV_rel['time']})
+                                        dims=('time',),
+                                        coords={'time': sV_rel['time']})
 
     # HORNET
     pE_rel['HORNET'] = (-1e15 * barycentric_avg(n_M)
@@ -714,7 +714,11 @@ def agyrotropy(P, b):
     return Q
 
 
-def permutation_entropy(x, n):
+def jenson_complexity():
+    pass
+
+
+def permutation_entropy(x, d, tau=1):
     '''
     
     Parameters
@@ -724,16 +728,21 @@ def permutation_entropy(x, n):
     n : 
         Embedding dimension
     '''
-    n_windows = len(x) - n + 1
-    total_permutations = np.math.factorial(n)
+    if d <= 1:
+        raise ValueError('d must be > 1, not {0}'.format(d))
+    if tau <= 0:
+        raise ValueError('tau must be > 0, not {0}'.format(tau))
 
-    permutations = list(itertools.permutations(np.arange(n), n))
+    n_windows = len(x) - (d - 1)*tau
+    total_permutations = np.math.factorial(d)
+
+    permutations = list(itertools.permutations(np.arange(d), d))
 
     # Sliding window iterable
     #    https://stackoverflow.com/questions/6822725/rolling-or-sliding-window-iterator
     occurrence = np.zeros(total_permutations)
-    for i in range(n_windows):
-        symbol = tuple(np.argsort(x[i:i+n])) # pi
+    for i in range(0, n_windows, tau):
+        symbol = tuple(np.argsort(x[i:i+d])) # pi
         idx = permutations.index(symbol)
         occurrence[idx] += 1 #
 
@@ -745,3 +754,177 @@ def permutation_entropy(x, n):
                 * np.log2(probability, np.zeros_like(probability), where=probability!=0))
 
     return H
+
+
+def permutation_entropy_2(x, d, tau=1):
+
+
+    partitions = np.lib.stride_tricks.sliding_window_view(
+                    x,
+                    window_shape = (d+(d-1)*(tau-1),),
+                    axis = 0
+    )
+    partitions[:,::tau]
+    partitions = np.argsort(partitions, axis=1).T
+    symbols, symbol_count = np.unique(partitions, return_counts=True, axis=0)
+    probability = symbol_count / symbol_count.sum()
+    entropy = -np.sum(probability * np.log2(probability))
+    norm_entropy = entropy / np.log2(np.math.factorial(d))
+
+    return norm_entropy
+
+
+def main(data):
+    
+
+    # Determine sample size, etc.
+    N = len(data)
+    d = 2
+    tau = 1
+    n_segs = N - (d - 1) * tau
+    '''
+    print('Samples:             {0:10d}'.format(N))
+    print('Embedding delay:     {0:10d}'.format(tau))
+    print('Embedding dimension: {0:10d}'.format(d))
+    print('Segments:            {0:10d}'.format(n_segs))
+    print('Segment Ratio:       {0:0.4f}'.format(n_segs / N))
+    '''
+
+    d_max = 6
+    tau_max = N // 100
+    n_segs_min = N - (d_max - 1) * tau_max
+
+    H = dict()
+    for d in range(2, 8):
+        pe = [permutation_entropy_2(data, d, tau=tau)
+              for tau in range(1, tau_max)]
+        H[d] = pe
+
+    return H
+    
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    import plots
+    import datetime as dt
+    from pymms.sdc import selections as sel
+    from pymms.data import fgm
+
+    import ordpy
+
+    '''
+    2023-02-27/16:33:23 - 2023-02-27/16:36:23, 105.0, yqi(EVA),  unbiased magnetosheath campaign
+    2023-02-27/16:42:13 - 2023-02-27/16:45:13, 105.0, yqi(EVA),  unbiased magnetosheath campaign
+    2023-02-27/16:51:03 - 2023-02-27/16:54:03, 105.0, yqi(EVA),  unbiased magnetosheath campaign
+    2023-02-27/16:59:53 - 2023-02-27/17:02:53, 105.0, yqi(EVA),  unbiased magnetosheath campaign
+    2023-02-27/17:08:43 - 2023-02-27/17:11:43, 105.0, yqi(EVA),  unbiased magnetosheath campaign
+    2023-02-27/17:17:33 - 2023-02-27/17:20:33, 105.0, yqi(EVA),  unbiased magnetosheath campaign
+    2023-02-27/17:26:23 - 2023-02-27/17:29:23, 105.0, yqi(EVA),  unbiased magnetosheath campaign
+    '''
+    t0 = dt.datetime(2023, 2, 27, 16, 33, 23)
+    t1 = dt.datetime(2023, 2, 27, 17, 29, 24)
+
+    '''
+    2023-03-24/04:42:53 - 2023-03-24/04:45:53, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/04:51:53 - 2023-03-24/04:54:53, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/05:00:53 - 2023-03-24/05:03:53, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/05:09:43 - 2023-03-24/05:12:43, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/05:18:43 - 2023-03-24/05:21:43, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/05:27:43 - 2023-03-24/05:30:43, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/05:36:43 - 2023-03-24/05:39:43, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/05:45:43 - 2023-03-24/05:48:43, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/05:54:33 - 2023-03-24/05:57:33, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/06:03:33 - 2023-03-24/06:06:33, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/06:12:33 - 2023-03-24/06:15:33, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/06:21:33 - 2023-03-24/06:24:33, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/06:30:33 - 2023-03-24/06:33:33, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/06:39:23 - 2023-03-24/06:42:23, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    2023-03-24/06:48:23 - 2023-03-24/06:51:23, 105.0, sraptis(EVA), unbiased magnetosheath campaign
+    '''
+    # t0 = dt.datetime(2023, 3, 24, 4, 42, 53)
+    # t1 = dt.datetime(2023, 3, 24, 6, 51, 24)
+
+    # Define a time interval
+    #   - First segment in unbiased magnetosheath campaign
+    #   - Near bow shock
+    # https://lasp.colorado.edu/mms/sdc/public/plots/#/quicklook?plot_type=all_mms1_summ&year=2024&month=04&day=06&time=0400_0120
+    # https://lasp.colorado.edu/mms/sdc/public/about/events/#/show/284037
+    # Time interval of unbiased magnetosheath campaign
+    selections = sel.selections('sitl', t0, t1, combine=True, sort=True, filter='unbiased')
+
+    # Grab the survey data
+    # b_data = fgm.load_data(sc='mms4', mode='srvy', start_date=t0, end_date=t1)
+    # data = b_data['B_GSE'].loc[:,'x']
+
+    # Create the figure
+    ncols = int(np.ceil(np.sqrt(len(selections))))
+    nrows = ncols
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(7,7), squeeze=False)
+    plt.subplots_adjust(left=0.08, right=0.9, top=0.97, bottom=0.07, wspace=0.1, hspace=0.1)
+
+    entropy = np.zeros(len(selections))
+    complexity = np.zeros(len(selections))
+    for idx, segment in enumerate(selections):
+        irow = idx // ncols
+        icol = idx % nrows
+
+        # Select the subset of time
+        # ts = segment.tstart
+        # te = segment.tstop
+        # data = b_data['B_GSE'].loc[:,'x'].sel(time=slice(ts, te))
+        ts = segment.tstart.astype(dt.datetime)
+        te = segment.tstop.astype(dt.datetime)
+        b_data = fgm.load_data(sc='mms4', mode='brst', start_date=ts, end_date=te)
+        data = b_data['B_GSE'].loc[:,'x']
+
+        # Permutation entropy as a function of embedding delay
+        H_tau = dict()
+        for d in range(2, 9):
+            H_tau[d] = [ordpy.permutation_entropy(data, dx=d, taux=tau)
+                        for tau in range(int(len(data)/100))]
+
+        # Compute the entropy
+        H, C = ordpy.entropy_complexity(data, dx=3)
+        entropy[idx] = H
+        complexity[idx] = C
+
+        # Plot H(tau)
+        ax = axes[irow,icol]
+        for key, value in H_tau.items():
+            ax.plot(np.arange(1, len(value)+1), value, label='d={0}'.format(key))
+        ax.set_ylabel('H')
+        ax.set_xlabel('$\\tau$')
+
+        # Add a legend to the upper-right panel
+        if (irow == 0) & (icol == ncols-1):
+            plots.add_legend(ax, outside=True)
+        
+        # Turn off y-labels for all but the left column
+        if icol != 0:
+            ax.set_ylabel('')
+            ax.set_yticklabels('')
+        
+        # Turn off x-labels for all but the bottom row
+        if irow != nrows-1:
+            ax.set_xlabel('')
+            ax.set_xticklabels('')
+
+    import pdb
+    pdb.set_trace()
+
+    # Plot C(H)
+    fig, axes = plt.subplots(nrows=1, ncols=1, squeeze=False)
+    ax = axes[0,0]
+    ax.plot(C, H)
+    ax.plot(HC_min)
+    ax.plot(HC_max)
+
+    # Theoretical minimum and maximum entropy-complexity curves
+    HC_min = ordpy.minimum_complexity_entropy(dx=3)
+    HC_max = ordpy.maximum_complexity_entropy(dx=3)
+
+    plt.show(block=False)
+    import pdb
+    pdb.set_trace()
+    
+